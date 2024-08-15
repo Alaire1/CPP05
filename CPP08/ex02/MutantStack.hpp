fd4dd76ed4b3fa73361a3e154db79b8a8580329f
Alaire <akaraban@student.42wolfsburg.de>
2024-08-15 14:54:09 +0200

    ex00 and ex02 cpp08

---
+++ CPP08/ex02/MutantStack.hpp
@@ -0,0 +1,20 @@
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+# include <stack>
+
+template <typename T>
+class MutantStack: public std::stack<T> {
+public:
+    MutantStack();
+    MutantStack(MutantStack const &src);
+    MutantStack &operator=(MutantStack const &src);
+    ~MutantStack();
+
+    typedef typename std::stack<T>::container_type::iterator iterator;
+    iterator begin();
+    iterator end();
+};
+#include "MutantStack.tpp"
+
+#endif
